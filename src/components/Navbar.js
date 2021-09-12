942a06a90bec829b66550369aca219bf1a75e04c
LalitN <lalit@mobiux.in>
2021-09-12 16:36:08 -0400

    1. Added login and register links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,30 +8,36 @@ import { Link, BrowserRouter as Router, Switch, Route } from "react-router-dom";
 export default function Navbar() {
     return (
         <Router>
-        <div>
-            <nav>
-            <ul>
-                <li><Link to="/home">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact Us</Link></li>
-                <li><Link to="/posts">Posts</Link></li>
-            </ul>
-        </nav>
-        <Switch>
-            <Route path="/home">
-                <Home />
-            </Route>
-            <Route path="/about">
-                <About />
-            </Route>
-            <Route path="/contact">
-                <Contact />
-            </Route>
-            <Route path="/posts">
-                <Posts />
-            </Route>
-        </Switch>
-        </div>
+            <div>
+                <nav className="main-nav">
+                    <ul>
+                        <li><Link to="/home">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/contact">Contact Us</Link></li>
+                        <li><Link to="/posts">Posts</Link></li>
+                    </ul>
+                </nav>
+                <div className="login-nav">
+                    <ul>
+                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/register">Register</Link></li>
+                    </ul>
+                </div>
+            <Switch>
+                <Route path="/home">
+                    <Home />
+                </Route>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/contact">
+                    <Contact />
+                </Route>
+                <Route path="/posts">
+                    <Posts />
+                </Route>
+            </Switch>
+            </div>
         </Router>
     )
 }
